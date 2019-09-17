f6d6380565d594b116fb2ccfcd01d3091ddce438
Pr0Vlad <55094161+Pr0Vlad@users.noreply.github.com>
2019-09-17 18:26:44 -0400

    Update Client1.java

--- Client1.java
+++ Client1.java
@@ -12,6 +12,7 @@ import java.net.*;
 public class Client1 implements Runnable {
 	static int choice;
 	static int ttime;
+	static int ttime2 = 0;
 	Client1(int choice) {
 		this.choice = choice;
 		
@@ -25,7 +26,7 @@ public class Client1 implements Runnable {
 		// scanner for input
 		Scanner scan2 = new Scanner(System.in);
 		do {
-			int ttime = 0;
+			ttime2 = 0;
 			System.out.println("1: Host current Date and Time \n" + "2: Host Uptime \n" + "3: Host memory use \n"
 					+ "4: Host Netstat \n" + "5: Host current users \n" + "6: Host running processes \n"
 					+ "7: Quit \n");
@@ -50,14 +51,19 @@ public class Client1 implements Runnable {
 					e.printStackTrace();
 				}
 			}
-			System.out.println(ttime + "milli ");
+			System.out.println(ttime2/choice + "milli ");
 		} while (choice != 7);
 	}
+	
+	public void setT (int ttime) {
+		this.ttime2 += ttime;
+	}
+	
 	@Override
 	public void run() {
 		try {
 			InetAddress ip = InetAddress.getByName("192.168.101.101");
-			Socket sock = new Socket(ip, 4144);
+			Socket sock = new Socket(ip, 4145);
 			long time = System.currentTimeMillis();
 			long end;
 			System.out.println(time);
@@ -67,7 +73,7 @@ public class Client1 implements Runnable {
 			// reading from server
 			send.println(choice + "\n");
 			send.flush();
-			System.out.println(re.readLine());
+			//System.out.println(re.readLine());
 			// loop to keep selecting options as long as 7 is not pressed
 			String line = new String();
 			// switch for different print outs based on selection cuz some need a loop
@@ -76,16 +82,19 @@ public class Client1 implements Runnable {
 				System.out.println(re.readLine());
 				 end = System.currentTimeMillis();
 				ttime += end - time ;
+				this.setT(ttime);
 				break;
 			case 2:
 				System.out.println(re.readLine());
 				 end = System.currentTimeMillis();
-				 ttime += end - time ;
+				 ttime += end - time;
+				 this.setT(ttime);
 				break;
 			case 3:
 				System.out.println(re.readLine());
 				 end = System.currentTimeMillis();
 				 ttime += end - time ;
+				 this.setT(ttime);
 				break;
 			case 4:
 				while ((line = re.readLine()) != null) {
@@ -96,6 +105,7 @@ public class Client1 implements Runnable {
 				}
 				 end = System.currentTimeMillis();
 				 ttime += end - time ;
+				 this.setT(ttime);
 				break;
 			case 5:
 				while ((line = re.readLine()) != null) {
@@ -106,6 +116,7 @@ public class Client1 implements Runnable {
 				}
 				end = System.currentTimeMillis();
 				ttime += end - time ;
+				this.setT(ttime);
 				break;
 			case 6:
 				while ((line = re.readLine()) != null) {
@@ -116,11 +127,13 @@ public class Client1 implements Runnable {
 				}
 				 end = System.currentTimeMillis();
 				 ttime += end - time ;
+				 this.setT(ttime);
 				break;
 			case 7:
 				sock.close();
 				 end = System.currentTimeMillis();
 				 ttime += end - time ;
+				 this.setT(ttime);
 				break;
 			// if wrong entry lets u go again
 			}
